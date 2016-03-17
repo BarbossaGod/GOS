cbee536bc0047f793b3a79ed1e52538479f7a5a1
Farscape2000 <jamievanzijl01082000@gmail.com>
2016-03-17 12:08:37 +0100

    Update BlitzcrankVer I.lua

--- BlitzcrankVer I.lua
+++ BlitzcrankVer I.lua
@@ -22,7 +22,7 @@ OnTick(function (myHero)
 				
 				            local RPred = GetPredictionForPlayer(GetOrigin(myHero), target, GetMoveSpeed(target), 1750, 25, 925, 231.25, false, true)
                             	if RPred.HitChance == 1 then
-                                     CastSkillShot(_R,RPred.PredPos)								
+                                     CastSkillShot(_Q,RPred.PredPos)								
 				end
 				
 				if BlitzcrankMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 1000) then
