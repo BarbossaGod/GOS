9aab1d0a9ea1dd1819b82d0358a324b68f686ac6
Farscape2000 <jamievanzijl01082000@gmail.com>
2016-05-24 00:58:55 +0200

    Update Nautilus.lua

--- Nautilus.lua
+++ Nautilus.lua
@@ -6,7 +6,7 @@ function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         PrintChat("New version found! " .. data)
         PrintChat("Downloading update, please wait...")
-        DownloadFileAsync("", SCRIPT_PATH .. "Nautilus", function() PrintChat("Update Complete, please 2x F6!") return end)
+        DownloadFileAsync("https://raw.githubusercontent.com/Farscape2000/GOS/master/Nautilus.lua", SCRIPT_PATH .. "Nautilus.lua", function() PrintChat("Update Complete, please 2x F6!") return end)
     else
         PrintChat("No updates found!")
     end
